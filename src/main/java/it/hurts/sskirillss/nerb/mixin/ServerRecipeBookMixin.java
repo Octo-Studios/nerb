095cefcf56578d6c1171bc19858b55547da25bb7
SSKirillSS <SSTroveSS@Gmail.com>
2022-12-19 12:44:04 +0300

    Fixed JEI compatibility

--- src/main/java/it/hurts/sskirillss/nerb/mixin/ServerRecipeBookMixin.java
+++ src/main/java/it/hurts/sskirillss/nerb/mixin/ServerRecipeBookMixin.java
@@ -1,13 +1,18 @@
 package it.hurts.sskirillss.nerb.mixin;
 
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.item.crafting.ServerRecipeBook;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.play.server.SRecipeBookPacket;
+import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(ServerRecipeBook.class)
 public class ServerRecipeBookMixin {
@@ -18,6 +23,11 @@ public class ServerRecipeBookMixin {
 
     @Inject(method = "fromNbt", at = @At("HEAD"), cancellable = true)
     public void onLoad(CompoundNBT tag, RecipeManager manager, CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "sendRecipes", at = @At("HEAD"), cancellable = true)
+    public void onLoad(SRecipeBookPacket.State state, ServerPlayerEntity player, List<ResourceLocation> recipes, CallbackInfo ci) {
         ci.cancel();
     }
 }
