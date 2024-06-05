ba3823d614e486c6a8e5cb557f1974f045704357
SSKirillSS <SSTroveSS@Gmail.com>
2024-06-05 22:17:42 +0300

    Initial port to 1.20.6

--- common/src/main/java/it/hurts/sskirillss/nerb/mixin/ScreenMixin.java
+++ common/src/main/java/it/hurts/sskirillss/nerb/mixin/ScreenMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
-    @Inject(method = "addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "addRenderableWidget", at = @At("HEAD"), cancellable = true)
     public <T extends GuiEventListener & Renderable & NarratableEntry> void onWidgetAdded(T widget, CallbackInfoReturnable<T> cir) {
         if (widget instanceof ImageButton image && image.sprites != null && image.sprites.enabled().equals(new ResourceLocation("recipe_book/button")))
             cir.setReturnValue(null);
